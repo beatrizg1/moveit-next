e0ca55b8926fd66cf6ecd696803b3fb44820a4d5
Beatriz Gonzaga <beatriz.gonzaga@outlook.com.br>
2021-03-10 10:50:31 -0300

    :pencil: Nova atualização

--- src/components/SideBar.tsx
+++
@@ -1,8 +0,0 @@
-import styles from '../styles/components/SideBar.modules.css'
-
-export function SideBar() {
-
-  <div className={styles.sideBarContainer}>
-    <img src="./public/logo-full.svg" alt="Logo moveit"/>
-  </div>
-} 
